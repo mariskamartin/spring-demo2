8b7a751f82ee66a24efdd364ab9857833c43d7be
Mariska Martin <mmariska@vendavo.com>
2018-01-23 16:25:57 +0100

    try logback in json format

--- src/main/java/com/mmariska/springdemo2/SpringDemo2Application.java
+++ src/main/java/com/mmariska/springdemo2/SpringDemo2Application.java
@@ -1,5 +1,7 @@
 package com.mmariska.springdemo2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +11,14 @@ import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class SpringDemo2Application {
+	private static final Logger log = LoggerFactory.getLogger(SpringDemo2Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemo2Application.class, args);
+		log.error("hello");
+		log.warn("panda");
+		log.info("is");
+		log.debug("happy");
 	}
 
     /**
