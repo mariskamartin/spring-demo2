e52fc6705e198c970f9267fa578cc683b53df00d
Mariska Martin <mmariska@vendavo.com>
2018-04-23 08:18:01 +0200

    add Perf endpoints for perf. container testing

--- src/main/java/com/mmariska/springdemo2/PerfController.java
+++ src/main/java/com/mmariska/springdemo2/PerfController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.ref.WeakReference;
 import java.time.temporal.UnsupportedTemporalTypeException;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * this is for spring concurrency reasons
@@ -21,6 +27,70 @@ public class PerfController {
     private static final Logger log = LoggerFactory.getLogger(PerfController.class);
     private int sum = 0;
 
+    @RequestMapping(value="/eatAllCPU", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
+    private String eatAllCPU() throws InterruptedException {
+        return eatAllCPUWithParams(5000);
+    }
+
+    @RequestMapping(value="/eatAllCPU/time/{timeMillis}", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
+    private String eatAllCPUWithParams(@PathVariable("timeMillis") int timeMillis) throws InterruptedException {
+        final long s = System.currentTimeMillis();
+        final int availableProcessors = Runtime.getRuntime().availableProcessors();
+        final int runTimeoutMillis = timeMillis;
+        ExecutorService executor = Executors.newFixedThreadPool(availableProcessors);
+        for (int i = 0; i < availableProcessors; i++) {
+            executor.submit(() -> {
+                long sum = 0;
+                while(true) {
+                    if (Thread.currentThread().isInterrupted()) return 0;
+                    if((System.currentTimeMillis() - s) > runTimeoutMillis) return sum;
+                    sum += Math.round(Math.random()*100);
+                }
+            });
+        }
+        executor.shutdown();
+        if (!executor.awaitTermination(runTimeoutMillis * 2, TimeUnit.SECONDS)) {
+            executor.shutdownNow();
+        }
+        return "done availableProcessors = " + availableProcessors + ", elapsed = " + (System.currentTimeMillis() - s) + " ms";
+    }
+
+
+    private static final Vector v = new Vector();
+
+    @RequestMapping(value="/eatMemory/{countMB}", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
+    private String eatMemory(@PathVariable() int countMB) {
+        int oneMiB = 1024 * 1024;
+        while (--countMB > 0)
+        {
+            byte b[] = new byte[oneMiB];
+            v.add(b);
+        }
+        return getCurrentMemoryStats(oneMiB, "MiB");
+    }
+
+    @RequestMapping(value="/eatMemory/weak/{countMB}", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
+    private String eatMemoryWeak(@PathVariable() int countMB) {
+        int oneMiB = 1024 * 1024;
+        while (--countMB > 0)
+        {
+            byte b[] = new byte[oneMiB];
+            v.add( new WeakReference<Object>(v));
+        }
+        return getCurrentMemoryStats(oneMiB, "MiB");
+    }
+
+
+    private String getCurrentMemoryStats(int formatSize, String size) {
+        Runtime runtime = Runtime.getRuntime();
+        long used = runtime.totalMemory() - runtime.freeMemory();
+        return String.format("objs=%d, [size %s] total=%d, free=%d, used=%d, max=%d",v.size(),
+                size,
+                runtime.totalMemory()/formatSize,
+                runtime.freeMemory()/formatSize,
+                used/formatSize,
+                runtime.maxMemory()/formatSize);
+    }
 
     @RequestMapping(value="/sum1", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
     private String sum1() {
@@ -36,6 +106,15 @@ public class PerfController {
 
     @RequestMapping(value="/sum/{count}/sleep/{sleep}", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
     private String sum(@PathVariable("count") int count, @PathVariable("sleep") int sleep) {
+        for (int i = 0; i < count; i++) {
+            if(sleep >= 0) sleep(sleep);
+            sum += Math.round(Math.random()*100);
+        }
+        return "done";
+    }
+
+    @RequestMapping(value="/parallelSum/{count}/sleep/{sleep}", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
+    private String parallelSum(@PathVariable("count") int count, @PathVariable("sleep") int sleep) {
         for (int i = 0; i < count; i++) {
             if(sleep >= 0) sleep(sleep);
             sum += Math.round(Math.random()*100);
